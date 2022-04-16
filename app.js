bbd42107a9b587a4b2090aeed2b5ace02cdb73c1
Muhammed Nishad <nishadmhdtk@icloud.com>
2022-04-16 22:44:24 +0530

    [UPDT] created server

--- app.js
+++ app.js
@@ -1,7 +1,8 @@
+const http = require('http');
 const Logger = require('./logger');
+
 const logger = new Logger();
 
-logger.on('messageLogged', (arg) => {
-    console.log("Event has been logged",arg);
-});
-logger.log('message');
+const server = http.createServer();
+server.listen(3000);
+logger.log("Server running at port 3000")
--- logger.js
+++ logger.js
@@ -1,10 +1,10 @@
 const EventEmitter = require('events');
 
 class Logger extends EventEmitter {
-log(message){
-    // TODO: Send an HTTP request to the log server
-    console.log(message);
-    this.emit('messageLogged', {id: 1, url: 'http://'});
-}
+    log(message){
+        // TODO: Send an HTTP request to the log server
+        console.log(message);
+        this.emit(`${Date.now()}messageLogged`, {id: 1, url: 'http://'});
+    }
 }
 module.exports= Logger;
